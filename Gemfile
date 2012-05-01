f1f3279d495680079dbafd68f19fc366f4d87aa6
Derek Kastner <dkastner@gmail.com>
2012-04-30 23:14:35 -0400

    Rails app (out-hipstering the hipsters)

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'devise'
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'nodeify'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captainplanet::Application.config.secret_token = 'c8f40fc7a84cafea95fee560cbf58a3b84ec546f3e8daa8f25c52c6368e50cc6d580b5bf884f64f3c59063c34877ea197f6401507b2dd7b25b745f1099efa3d7'
